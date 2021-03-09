f8167034cb5e28d93986cc9f072945a4cbdd6788
MohamedCharaabi <medch373@gmail.com>
2021-03-09 18:26:50 +0100

    auth

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -51,7 +51,7 @@ router.post('/login', async (req, res) => {
 
     //store the token on cookies
     res.cookie('jwt', token, {
-        httpOnly: false,
+        httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000, //1 day
         // sameSite: 'lax',
         // secure: true
